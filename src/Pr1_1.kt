37c541533ea13d8f5cca5adf896f34fa207da610
Riddhi <riddhisiddhi.166747@gmail.com>
2026-06-22 14:31:27 +0530

    init

---
+++ src/Pr1_1.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var i=1
+    var j:Int=2
+    var k:Float=1.5F
+    var l:Char='T'
+    var m:String="Riddhi"
+    var n:Boolean=false
+    var o:Double=55.55
+    var p:Long=1122334455
+    var q:Short=-2
+    var r:Byte=125
+    println("r="+r)
+    println("q="+q)
+    println("p="+p)
+    println("o="+o)
+    println("n="+n)
+    println("m="+m)
+    println("l="+l)
+    println("k="+k)
+    println("j="+j)
+    println("i="+i)
+}
